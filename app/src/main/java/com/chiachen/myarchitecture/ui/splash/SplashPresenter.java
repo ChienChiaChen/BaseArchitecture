6bb3f4cfc60c2eef066e52a97c6d7c56bac8d02b
JasonChien <s124202468@gmail.com>
2018-05-30 16:38:43 +0800

    Getting user data(haha..)

--- app/src/main/java/com/chiachen/myarchitecture/ui/splash/SplashPresenter.java
+++ app/src/main/java/com/chiachen/myarchitecture/ui/splash/SplashPresenter.java
@@ -2,6 +2,7 @@ package com.chiachen.myarchitecture.ui.splash;
 
 import com.chiachen.myarchitecture.base.BasePresenterImpl;
 import com.chiachen.myarchitecture.data.DataManager;
+import com.chiachen.myarchitecture.data.db.user.User;
 import com.chiachen.myarchitecture.data.network.ApiCallback;
 import com.chiachen.myarchitecture.utils.rx.SchedulerProvider;
 
@@ -9,14 +10,19 @@ import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
+import io.bloco.faker.Faker;
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 
 public class SplashPresenter<V extends SplashMvpView> extends BasePresenterImpl<V> implements SplashMvpPresenter<V> {
 
     @Inject
     public SplashPresenter(SchedulerProvider schedulerProvider, CompositeDisposable compositeDisposable, DataManager dataManager) {
         super(schedulerProvider, compositeDisposable, dataManager);
+
     }
 
     @Override
@@ -27,11 +33,27 @@ public class SplashPresenter<V extends SplashMvpView> extends BasePresenterImpl<
     public void loadData() {
         getMvpView().showLoading();
         addSubscription(
-                Observable.empty().delay(3, TimeUnit.SECONDS),
-                new ApiCallback<Integer>() {
+                Observable.create(new ObservableOnSubscribe<User>() {
                     @Override
-                    public void onSuccess(Integer integer) {
-                        getMvpView().onError(String.valueOf(integer));
+                    public void subscribe(ObservableEmitter<User> e) throws Exception {
+                        Faker faker = new Faker();
+                        User user;
+                        user = new User();
+                        user.setEmail(faker.internet.email("JasonFromTaiwan"));
+                        user.setName("JasonFromTaiwan");
+                        user.setAvatar(faker.avatar.image(user.getEmail()));
+                        e.onNext(user);
+                        e.onComplete();
+                    }
+                }).doOnNext(new Consumer<User>() {
+                    @Override
+                    public void accept(User user) throws Exception {
+                        getDataManager().addUser(user);
+                    }
+                }).delay(3, TimeUnit.SECONDS),
+                new ApiCallback<User>() {
+                    @Override
+                    public void onSuccess(User integer) {
                     }
 
                     @Override
