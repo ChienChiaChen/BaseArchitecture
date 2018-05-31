14d12b7107788fb9f542d7840d6d64d5562f44fc
JasonChien <s124202468@gmail.com>
2018-05-31 13:37:16 +0800

    Add fragment utils

---
+++ app/src/main/java/com/chiachen/myarchitecture/utils/FragmentUtils.java
@@ -0,0 +1,25 @@
+package com.chiachen.myarchitecture.utils;
+
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.chiachen.myarchitecture.R;
+
+public class FragmentUtils {
+    private FragmentUtils() {
+    }
+
+    public static void nextFragment(FragmentManager fragmentManager,
+                                    Fragment fragment,
+                                    String tag,
+                                    @IdRes int containerViewId)
+    {
+        fragmentManager
+                .beginTransaction()
+                .addToBackStack(tag)
+                .setCustomAnimations(R.anim.slide_left, R.anim.slide_right, R.anim.slide_left, R.anim.slide_right)
+                .add(containerViewId, fragment, tag)
+                .commit();
+    }
+}
