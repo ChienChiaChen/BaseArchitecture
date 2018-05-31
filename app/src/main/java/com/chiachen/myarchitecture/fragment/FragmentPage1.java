72f5c715060c7317fdabf616a64b6ad9e4c58e95
JasonChien <s124202468@gmail.com>
2018-05-31 13:38:55 +0800

    Add fragment 1.

---
+++ app/src/main/java/com/chiachen/myarchitecture/fragment/FragmentPage1.java
@@ -0,0 +1,34 @@
+
+
+package com.chiachen.myarchitecture.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chiachen.myarchitecture.R;
+
+
+public class FragmentPage1 extends Fragment {
+    public static final String TAG = "FragmentPage1";
+
+    public static FragmentPage1 newInstance() {
+        return new FragmentPage1();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page_1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
